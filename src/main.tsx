bcf98f5cef97e6b4f61811cfbf0ed4cf1d10fc53
webLXM <1944754630@qq.com>
2024-08-11 22:48:47 +0800

    feat(config): 配置路径别名

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+    // 不能自动检测副作用，有意双重调用
+  // <StrictMode>
     <App />
-  </StrictMode>,
+  // </StrictMode>,
 )
