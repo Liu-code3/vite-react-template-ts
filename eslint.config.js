cc6150edaa6bd101a6c1b075494f2a6ccf0aeb0b
webLXM <1944754630@qq.com>
2024-08-18 13:48:22 +0800

    fix(eslint): @antfu/eslint-config

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  react: true,
+  typescript: true,
+  formatters: true,
+  unocss: true,
+  stylistic: {
+    indent: 2, // 2, or 'tab'
+    quotes: 'single', // or 'double'
+  },
+  ignores: [
+    '.vscode',
+    'build/',
+    'src/assets/',
+    'public/',
+    'dist/',
+    'node_modules/',
+    'pnpm-lock.yaml',
+    '**/*.d.ts',
+  ],
+})
