fdc3f067a5e029e04a943da1aa10302371fe012c
webLXM <1944754630@qq.com>
2024-08-26 23:58:45 +0800

    fix(router): 使用懒加载路由的时候 切换页面闪烁问题

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,14 +1,23 @@
-import { lazy } from 'react'
+import { Suspense, lazy } from 'react'
+import type { ReactNode } from 'react'
 import type { RouteObject } from 'react-router-dom'
 import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 const HelloWorld = lazy(() => import('@/views/HelloWorld.tsx'))
 const NotFound = lazy(() => import('@/views/NotFount/404.tsx'))
 
+// 闪烁的发生是因为将整个 App 包裹在 Suspense 中会导致在懒加载时整个应用都渲染 fallback。而将 Suspense 包裹在单个组件上，只会让正在懒加载的组件局部渲染 fallback，从而避免了不必要的全屏重新渲染并防止闪烁。
+function lazyComponent(element: ReactNode): ReactNode {
+  return (
+    <Suspense fallback={<div>loading...</div>}>
+      { element }
+    </Suspense>
+  )
+}
 const routes: RouteObject[] = [
   { path: '/', element: <Navigate to="/home" /> },
-  { path: '/home', element: <HelloWorld /> },
-  { path: '*', element: <NotFound /> }
+  { path: '/home', element: lazyComponent(<HelloWorld />) },
+  { path: '*', element: lazyComponent(<NotFound />) },
 ]
 
 /** https://blog.csdn.net/AXBNMD/article/details/137468854 使用lazy的话，需向如下这样操作 */
